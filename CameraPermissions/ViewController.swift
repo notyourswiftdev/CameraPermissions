047b3e18a9fc823270ee9c883bf57cbade9de181
Aaron Cleveland <toscleveland@gmail.com>
2021-04-07 20:26:13 -0500

    Added label and imageview

--- CameraPermissions/ViewController.swift
+++ CameraPermissions/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let chooseImageViewHeight: CGFloat = 80
     
+    let chooseImageTitle: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Choose an Image"
+        label.font = .boldSystemFont(ofSize: 16)
+        return label
+    }()
+    
+    let chooseImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.borderWidth = 1
+        imageView.layer.borderColor = UIColor.blue.cgColor
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
+        view.addSubview(chooseImageTitle)
+        NSLayoutConstraint.activate([
+            chooseImageTitle.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            chooseImageTitle.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        view.addSubview(chooseImageView)
+        NSLayoutConstraint.activate([
+            chooseImageView.centerYAnchor.constraint(equalTo: chooseImageTitle.centerYAnchor),
+            chooseImageView.centerXAnchor.constraint(equalTo: chooseImageTitle.centerXAnchor),
+            chooseImageView.heightAnchor.constraint(equalToConstant: chooseImageViewHeight),
+            chooseImageView.widthAnchor.constraint(equalToConstant: chooseImageViewHeight)
+        ])
     }
 
 
