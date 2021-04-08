f8872e6ac583592413130599d10436167fe83528
Aaron Cleveland <toscleveland@gmail.com>
2021-04-07 19:48:39 -0500

    Initial Commit

---
+++ CameraPermissions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraPermissions
+//
+//  Created by Aaron Cleveland on 4/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
