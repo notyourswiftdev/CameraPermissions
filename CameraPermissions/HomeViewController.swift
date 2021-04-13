7c8f014118ffedf3030bf487f3b9eeaea651bfe2
Aaron Cleveland <toscleveland@gmail.com>
2021-04-13 02:16:02 -0500

    Added collectionview

--- CameraPermissions/HomeViewController.swift
+++ CameraPermissions/HomeViewController.swift
@@ -9,7 +9,8 @@ import UIKit
 import AVFoundation
 
 class HomeViewController: UIViewController {
-    // MARK: - Properties -
+    
+    // MARK: - PROPERTIES -
     let imagePicker = ImagePickerController()
     let filterImageViewController = FilterImageViewController()
     
@@ -17,28 +18,17 @@ class HomeViewController: UIViewController {
     let mainImageSize: CGFloat = 200
     let standardPadding: CGFloat = 20
     
-    lazy var chooseImageLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Choose \nImage"
-        label.font = .boldSystemFont(ofSize: 14)
-        label.numberOfLines = 0
-        label.textAlignment = .center
-        return label
+    lazy var photoCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: thumbnailSize, height: thumbnailSize)
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.backgroundColor = .clear
+        return collectionView
     }()
-    
-    lazy var thumbnailImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.contentMode = .scaleAspectFill
-        imageView.isUserInteractionEnabled = true
-        imageView.layer.cornerRadius = 10
-        imageView.clipsToBounds = true
-        return imageView
-    }()
-    
-    lazy var pictureSelectionTapGesture = UITapGestureRecognizer(target: self, action: #selector(chooseImageAlertAction))
-    lazy var filterImageSelectionTapGesture = UITapGestureRecognizer(target: self, action: #selector(filterImageAlertAction))
     
     lazy var mainImageView: UIImageView = {
         let imageView = UIImageView()
@@ -54,7 +44,7 @@ class HomeViewController: UIViewController {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setTitle("Filter Image", for: .normal)
-        button.backgroundColor = .red
+        button.backgroundColor = .systemTeal
         button.layer.cornerRadius = 10
         button.isHidden = true
         button.heightAnchor.constraint(equalToConstant: 40).isActive = true
@@ -62,13 +52,21 @@ class HomeViewController: UIViewController {
         return button
     }()
     
+    lazy var chooseImageButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Camera or Photo Library", for: .normal)
+        button.backgroundColor = .red
+        button.layer.cornerRadius = 10
+        button.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        button.addTarget(self, action: #selector(chooseImageButtonAction), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - LIFECYCLES -
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor(red: 245/255, green: 245/255, blue: 245/255, alpha: 1.0)
         configureUI()
-        checkCameraAuthorization()
-        
-        imagePicker.pickerDelegate = self
     }
     
     // Remove navigation bar
@@ -83,11 +81,18 @@ class HomeViewController: UIViewController {
         navigationController?.setNavigationBarHidden(false, animated: animated)
     }
     
-    // MARK: - Helper Functions -
+    // MARK: - HELPER FUNCTIONS -
     func configureUI() {
+        view.backgroundColor = UIColor(red: 245/255, green: 245/255, blue: 245/255, alpha: 1.0)
         constraints()
-        thumbnailImageView.addGestureRecognizer(pictureSelectionTapGesture)
-        mainImageView.addGestureRecognizer(filterImageSelectionTapGesture)
+        checkCameraAuthorization()
+        delegates()
+    }
+    
+    func delegates() {
+        imagePicker.pickerDelegate = self
+        photoCollectionView.delegate = self
+        photoCollectionView.dataSource = self
     }
     
     func checkCameraAuthorization() {
@@ -104,12 +109,7 @@ class HomeViewController: UIViewController {
         }
     }
     
-    func requestCameraPermission() {
-        AVCaptureDevice.requestAccess(for: .video) { (cameraGranted) in
-            guard cameraGranted == true else { return }
-        }
-    }
-    
+    // MARK: - PRESENTS CAMERA OR PHOTO LIBRARY OR EDIT IMAGE VIEW CONTROLLER -
     func presentCamera() {
         self.present(imagePicker, animated: true, completion: nil)
     }
@@ -123,6 +123,13 @@ class HomeViewController: UIViewController {
         navigationController?.pushViewController(self.filterImageViewController, animated: true)
     }
     
+    // MARK: - CAMERA PERMISSIONS HELPER FUNCTIONS -
+    func requestCameraPermission() {
+        AVCaptureDevice.requestAccess(for: .video) { (cameraGranted) in
+            guard cameraGranted == true else { return }
+        }
+    }
+    
     func alertCameraAccessNeeded() {
         let settingsAppURL = URL(string: UIApplication.openSettingsURLString)!
         let alert = UIAlertController(title: "Need Camera Access", message: "Camera access is required to make full use of this app.", preferredStyle: .alert)
@@ -133,16 +140,11 @@ class HomeViewController: UIViewController {
         present(alert, animated: true, completion: nil)
     }
     
-    // MARK: - Actions -
-    @objc func chooseImageAlertAction() {
+    // MARK: - ACTIONS -
+    @objc func chooseImageButtonAction() {
         checkCameraAuthorization()
-        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
         
-        if mainImageView.image != nil {
-            alert.addAction(UIAlertAction(title: "Choose Filter", style: .default, handler: { (_) in
-                self.presentFilterImageViewController()
-            }))
-        }
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Take Photo", style: .default, handler: { (_) in
             if !ImagePickerController.isSourceTypeAvailable(.camera) {
                 let alert  = UIAlertController(title: "Warning", message: "You don't have camera", preferredStyle: .alert)
@@ -165,43 +167,27 @@ class HomeViewController: UIViewController {
         self.present(alert, animated: true, completion: nil)
     }
     
-    @objc func filterImageAlertAction() {
-        if mainImageView.image != nil {
-            let alert = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "Choose Filter", style: .default, handler: { (_) in
-                self.presentFilterImageViewController()
-            }))
-            alert.addAction(UIAlertAction.init(title: "Cancel", style: .cancel, handler: nil))
-            self.present(alert, animated: true, completion: nil)
-        }
-    }
-    
     @objc func filterButtonAction() {
         self.presentFilterImageViewController()
     }
 }
 
+// MARK: - EXTENSIONS -
 extension HomeViewController {
     func constraints() {
-        view.addSubview(thumbnailImageView)
+        view.addSubview(photoCollectionView)
         NSLayoutConstraint.activate([
-            thumbnailImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: standardPadding),
-            thumbnailImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: standardPadding),
-            thumbnailImageView.heightAnchor.constraint(equalToConstant: thumbnailSize),
-            thumbnailImageView.widthAnchor.constraint(equalToConstant: thumbnailSize)
-        ])
-        
-        view.addSubview(chooseImageLabel)
-        NSLayoutConstraint.activate([
-            chooseImageLabel.centerXAnchor.constraint(equalTo: thumbnailImageView.centerXAnchor),
-            chooseImageLabel.centerYAnchor.constraint(equalTo: thumbnailImageView.centerYAnchor)
+            photoCollectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: standardPadding),
+            photoCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            photoCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            photoCollectionView.heightAnchor.constraint(equalToConstant: 100)
         ])
         
         view.addSubview(mainImageView)
         NSLayoutConstraint.activate([
-            mainImageView.topAnchor.constraint(equalTo: thumbnailImageView.bottomAnchor, constant: 20),
-            mainImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            mainImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            mainImageView.topAnchor.constraint(equalTo: photoCollectionView.bottomAnchor, constant: standardPadding),
+            mainImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: standardPadding),
+            mainImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -standardPadding),
             mainImageView.heightAnchor.constraint(equalToConstant: view.frame.height / 2)
         ])
         
@@ -211,18 +197,44 @@ extension HomeViewController {
             filterButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: standardPadding),
             filterButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -standardPadding)
         ])
+        
+        view.addSubview(chooseImageButton)
+        NSLayoutConstraint.activate([
+            chooseImageButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: standardPadding),
+            chooseImageButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -standardPadding),
+            chooseImageButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -standardPadding)
+        ])
     }
 }
 
 extension HomeViewController: ImagePickerControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let photo = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
-        thumbnailImageView.image = photo
         mainImageView.image = photo
-        
-        chooseImageLabel.isHidden = true
         filterButton.isHidden = false
         dismiss(animated: true, completion: nil)
     }
+}
+
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .darkGray
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("ENLARGE IMAGE")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        // Took away the leading and trailing constraint on the collectionview itself. added an edge inset here to give a better look to the view.
+        return UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+    }
+    
 }
 
